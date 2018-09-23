9da5dab921f63df93594ae02b4090effb5bbb178
MininMobile <notminin@pm.me>
2018-09-23 15:21:26 +0100

    add tooltips

--- src/behavior.js
+++ src/behavior.js
@@ -1,6 +1,34 @@
-// smooth scroll
+{ // tooltips
+	let tooltips = document.getElementsByClassName("tooltip");
+	let container = document.getElementById("tooltips-container");
 
-{
+	let tt = document.createElement("div");
+	tt.classList.add("tooltip-container", "hidden");
+
+	container.appendChild(tt);
+
+	[].forEach.call(tooltips, (tooltip) => {
+		let content = tooltip.getAttribute("title");
+
+		tooltip.addEventListener("pointerenter", () => {
+			tt.setAttribute("text", content);
+			tt.classList.remove("hidden");
+		});
+
+		tooltip.addEventListener("pointermove", (e) => {
+			tt.style.left = e.clientX + "px";
+			tt.style.top = e.clientY + "px";
+		});
+
+		tooltip.addEventListener("pointerleave", () => {
+			tt.classList.add("hidden");
+		});
+
+		tooltip.removeAttribute("title");
+	});
+}
+
+/* { // smooth scroll
 	smoothAnchor("crust");
 	smoothAnchor("mantle");
 	smoothAnchor("core-outer");
@@ -8,7 +36,7 @@
 	smoothAnchor("jump");
 
 	function smoothAnchor(link) {
-		let l = document.getElementById(`${link}-link`) || document.getElementById(link);
+		let l = document.getElementById(link + "-link") || document.getElementById(link);
 		let a = document.getElementById(l.getAttribute("href").substring(1));
 
 		l.removeAttribute("href");
@@ -16,13 +44,10 @@
 		l.addEventListener("click", () =>
 			a.scrollIntoView({ behavior: "smooth" })
 		);
-
 	}
-}
+} */
 
-// jump to top
-
-{
+{ // jump to top
 	let jump = document.getElementById("jump");
 
 	jump.classList.add("hidden");
@@ -35,5 +60,4 @@
 }
 
 // init particles.js
-
-particlesJS("particle-container", {"particles": {"number": {"value": 80,"density": {"enable": true,"value_area": 800}},"color": {"value": "#ffffff"},"shape": {"type": "circle","stroke": {"width": 0,"color": "#000000"},"polygon": {"nb_sides": 5},"image": {"src": "img/github.svg","width": 100,"height": 100}},"opacity": {"value": 0.5,"random": true,"anim": {"enable": false,"speed": 1,"opacity_min": 0.1,"sync": false}},"size": {"value": 3,"random": true,"anim": {"enable": false,"speed": 40,"size_min": 0.1,"sync": false}},"line_linked": {"enable": false,"distance": 150,"color": "#ffffff","opacity": 0.4,"width": 1},"move": {"enable": true,"speed": 0.2,"direction": "left","random": false,"straight": false,"out_mode": "out","bounce": false,"attract": {"enable": false,"rotateX": 600,"rotateY": 1200}}},"interactivity": {"detect_on": "window","events": {"onhover": {"enable": true,"mode": "grab"},"onclick": {"enable": false,"mode": "push"},"resize": true},"modes": {"grab": {"distance": 100,"line_linked": {"opacity": 0.8}},"bubble": {"distance": 400,"size": 40,"duration": 2,"opacity": 8,"speed": 3},"repulse": {"distance": 200,"duration": 0.4},"push": {"particles_nb": 4},"remove": {"particles_nb": 2}}},"retina_detect": true});
+particlesJS("particle-container", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":0.2,"direction":"left","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"window","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":100,"line_linked":{"opacity":0.8}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
